1f60178debbf860277abf1d9c4ace177673f3f8e
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-12-29 23:11:19 +0330

    jalase 3

--- jalase3/thirdApp/src/app/app.component.ts
+++ jalase3/thirdApp/src/app/app.component.ts
@@ -11,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'thirdApp';
+  name = 'reza';
+  c1 = "class1"
+  c2 = "class2"
+  myClass = 'm2';
+  flag = false
 }
---
+++ jalase3/typescript/variable.ts
@@ -0,0 +1,8 @@
+
+// variable
+
+let firstName: string = "reza";
+
+let lastName : string = "gholamalitabar"
+
+console.log(firstName + " " + lastName)
